1452d1d205737999fbfbfeeae51918c9aace0e47
ADevscode <absourcelink@gmail.com>
2024-06-30 11:11:15 +0000

    documentando a api

--- src/server.js
+++ src/server.js
@@ -2,13 +2,17 @@
 const express = require('express')
 const bodyParser =require('body-parser')
 const routa_estudante =require('../router/routa_estudante') 
+const swaggerUi = require('swagger-ui-express')
+
+const swaggerJson = require('./swagger.json')
 
 const app  = express()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(express.static('public'))
 app.set('view engine','ejs')
-
+//rota da documentação
+app.use('/api/docs',swaggerUi.serve,swaggerUi.setup(swaggerJson))
 //definindo as rotas da aplicação
 app.use('/',routa_estudante) 
 //
